17a5d2eb44c7be45678be353aef079151db70ff8
Gaojianli <823805669@qq.com>
2019-08-20 19:55:04 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bdcoder
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string commonArgs = " -threads 6 -vcodec libx264 -acodec aac -keyint_min 30 -preset slow -profile:v high10 -f mp4 ";
+        public PathData pathData;
+        public MainWindow()
+        {
+            pathData = new PathData() {
+                FFPath=$"{System.Environment.CurrentDirectory}\\ffmpeg.exe"//Default path
+            };
+            InitializeComponent();
+            ffpathBox.DataContext = pathData;
+            input_Box.DataContext = pathData;
+            output_Box.DataContext = pathData;
+        }
+        private void SelectFFButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var ffFilePicker = new System.Windows.Forms.OpenFileDialog()
+            {
+                Filter = "ffmpeg (*.exe)|*.exe",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            })
+            {
+                if (ffFilePicker.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pathData.FFPath = ffFilePicker.FileName;
+                }
+            }
+        }
+        private void DefaultConfButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ArgsBox.Text = $"{commonArgs} -g 120 -b:v 5000k -an -y NUL";
+            if(CutGroup?.Visibility==Visibility.Visible)
+                CutGroup.Visibility = Visibility.Hidden;
+        }
+        private void LongConfButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ArgsBox.Text = $"{commonArgs} -g 500 -b:v 3600k -an -y NUL";
+            if (CutGroup?.Visibility == Visibility.Visible)
+                CutGroup.Visibility = Visibility.Hidden;
+        }
+        private void CRFConfButton_Checked(object sender, RoutedEventArgs e)
+        {
+            ArgsBox.Text = $"{commonArgs} -crf 24 -g 120 -b:a 128ki";
+            if (CutGroup?.Visibility == Visibility.Visible)
+                CutGroup.Visibility = Visibility.Hidden;
+        }
+        private void SelectInputButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var filePicker = new System.Windows.Forms.OpenFileDialog()
+            {
+                Filter = "Supported Video File (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            })
+            {
+                if (filePicker.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pathData.InputFile = filePicker.FileName;
+                    var filename = pathData.InputFile.Substring(0, pathData.InputFile.LastIndexOf("."));
+                    var extName = pathData.InputFile.Substring(pathData.InputFile.LastIndexOf(".") + 1, (pathData.InputFile.Length - pathData.InputFile.LastIndexOf(".") - 1));
+                    pathData.OutputFile = $"{filename}_bdcoder.{extName}";
+                }
+            }
+        }
+        private void SelectOutputButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var filePicker = new System.Windows.Forms.SaveFileDialog()
+            {
+                InitialDirectory = pathData.OutputFile,
+                FileName= pathData.OutputFile,
+                Filter = $"Supported Video File (*.{pathData.InputFile.Substring(pathData.InputFile.LastIndexOf(".") + 1, (pathData.InputFile.Length - pathData.InputFile.LastIndexOf(".") - 1))})|*.{pathData.InputFile.Substring(pathData.InputFile.LastIndexOf(".") + 1, (pathData.InputFile.Length - pathData.InputFile.LastIndexOf(".") - 1))}",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            })
+            {
+                if (filePicker.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pathData.InputFile = filePicker.FileName;
+                }
+            }
+        }
+
+        private void PreviewButton_Click(object sender, RoutedEventArgs e)
+        {
+            string command = $"\"{pathData.FFPath}\"";
+            if ((bool)CUTConfButton.IsChecked)
+            {
+                var startTime = startTimePicker.Value;
+                var endTimeCmd = !(bool)endCheckBox.IsChecked ? $"-to {endTimePicker.Value}" : null;
+                command += $" -ss {startTime} {endTimeCmd} -accurate_seek -i \"{pathData.InputFile}\" -c copy -avoid_negative_ts 1 \"{pathData.OutputFile}\"";
+            }
+            else
+            {
+                command += $" -i \"{pathData.InputFile}\" ";
+                if ((bool)CRFConfButton.IsChecked)
+                    command += $"{ArgsBox.Text} \"{pathData.OutputFile}\"";
+                else
+                {
+                    command += $"{ArgsBox.Text} -pass 1";
+                    command += $" &{command} -pass 2 \"{pathData.OutputFile}\"";
+                }
+
+            }
+            if(MessageBox.Show($"Will excute \"{command}\"?","Confirm to excute?",MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                using (Process process = new Process())
+                {
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.UseShellExecute = true;
+                    process.StartInfo.CreateNoWindow = false;
+                    process.StartInfo.Arguments = $"/c \"{command} & pause\" ";
+                    process.Start();
+                }
+            }
+        }
+
+        private void CUTConfButton_Checked(object sender, RoutedEventArgs e)
+        {
+            CutGroup.Visibility = Visibility.Visible;
+            ArgsBox.IsEnabled = false;
+            ArgsBox.Text = "N/A";
+        }
+
+        private void EndCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            endTimePicker.IsEnabled = false;
+        }
+
+        private void EndCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            endTimePicker.IsEnabled = true;
+        }
+
+        private void TextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = true;
+        }
+
+        private void TextBox_PreviewDrop(object sender, DragEventArgs e)
+        {
+            foreach (var f in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                ((TextBox)sender).Text = f;
+            }
+        }
+    }
+}
---
+++ PathData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bdcoder
+{
+    public class PathData: INotifyPropertyChanged
+    {
+        private string inputFileName;
+        private string ffmpegPath;
+        private string outputFilePath;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string FFPath
+        {
+            get { return ffmpegPath; }
+            set
+            {
+                ffmpegPath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FFPath"));
+            }
+        }
+        public string InputFile
+        {
+            get { return inputFileName; }
+            set
+            {
+                inputFileName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("InputFile"));
+            }
+        }
+        public string OutputFile
+        {
+            get { return outputFilePath; }
+            set
+            {
+                outputFilePath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("OutputFile"));
+            }
+        }
+    }
+}
